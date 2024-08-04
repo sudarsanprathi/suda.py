a880f9f0ca8369b70d47a4615bb1508794d2afec
sudarsanprathi <138649413+sudarsanprathi@users.noreply.github.com>
2024-08-04 16:11:03 +0530

    extended code

--- suda.py
+++ suda.py
@@ -1,3 +1,12 @@
 print("hello")
 print("how are you")
 print("....")
+class computer:
+    def sai(self):
+        print("i am mr.sai")
+    def vasu(self):
+        print("i am vasu")
+com1=computer()
+com1.sai()
+com2=computer()
+com2.vasu()
